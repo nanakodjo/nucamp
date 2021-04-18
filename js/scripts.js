9ac242c20b80de92637ad2d0d10158dd5caa2eaf
Nana Kodjo Acquah-Asare <nanakodjo@gmail.com>
2021-04-18 14:42:34 -0400

    Week 4 Assignment Complete

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
+        });
+        $("#reserveButton").click(function(){
+             $("#reserveModal").modal("show");
+         });
+         $("#loginButton").click(function(){
+            $("#loginModal").modal("show");
+        });
+});
+
+
+// $(function(){
+//     $("#btnShowBanana").click(function(){
+//       $("#banana").collapse("show");
+//     });
+//     $("#btnShowMonkey").click(function(){
+//       $("#monkey").collapse("show");
+//     });
+//     $("#btnHideBanana").click(function(){
+//       $("#banana").collapse("hide");
+//     });
+//     $("#btnHideMonkey").click(function(){
+//       $("#monkey").collapse("hide");
+//     });
+//   });
